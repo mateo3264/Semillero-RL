c26fe0ee8fa3d8259d7c74e1c61bbe61f824d15d
Mateogit32 <36247420+mateo3264@users.noreply.github.com>
2021-03-30 21:31:50 -0500

    Update nonstationary_bandits_simple_comparing_agents.py
    Se editaron comentarios.

--- SESION_2/nonstationary_bandits/nonstationary_bandits_simple_comparing_agents.py
+++ SESION_2/nonstationary_bandits/nonstationary_bandits_simple_comparing_agents.py
@@ -1,22 +1,30 @@
-#Número variable de cambios de valor de acciones
-#Tabla (o graficos de desempeño según distintos hiperparametros
-# o cambios en el ambiente)
+#Agentes enfrentados a un problema en el que
+#los valores reales de las acciones (q(a))
+#cambian en un tiempo determinado por la
+#variable timestep_of_change. Los agentes
+#actualizan sus estimados con 1/n(a) y alpha constante
+#entre (0,1]. Se ve el desempeño de los dos
+#agentes en las gráficas.
 
 import numpy as np
 import matplotlib.pyplot as plt
 from comparing_agents import run_experiment
 
-np.random.seed(5) #para permitir replicación del experimento
+#para permitir replicación del experimento
+np.random.seed(5) 
 
-K = 5 #número de acciones posibles
+K = 5 
 
 epsilon = 0.1
+
 alpha = 0.01
+
 timesteps = 200_000
 
-timestep_of_change = 35000
+timestep_of_change = 35_000
 
 
 
 
 run_experiment(alpha,epsilon,timesteps,timestep_of_change,n_actions=K)
+
