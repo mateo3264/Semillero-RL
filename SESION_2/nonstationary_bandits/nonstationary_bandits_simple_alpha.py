62497d60e1ff56f07f21384c0c79ec51a071ed8d
Mateogit32 <36247420+mateo3264@users.noreply.github.com>
2021-03-30 21:22:26 -0500

    Update nonstationary_bandits_simple_alpha.py
    Se editaron los comentarios.

--- SESION_2/nonstationary_bandits/nonstationary_bandits_simple_alpha.py
+++ SESION_2/nonstationary_bandits/nonstationary_bandits_simple_alpha.py
@@ -1,6 +1,9 @@
-#Número variable de cambios de valor de acciones
-#Tabla (o graficos de desempeño según distintos hiperparametros
-# o cambios en el ambiente)
+#Agente enfrentado a un problema en el que
+#los valores reales de las acciones (q(a))
+#cambian en un tiempo determinado por la
+#variable timestep_of_change. El agente
+#actualiza sus estimados con alpha constante
+#entre (0,1].
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,14 +11,17 @@ from simple_alpha import run_experiment
 
 np.random.seed(2)
 
-K = 5 #número de acciones posibles
+#número de acciones posibles
+K = 5 #Modificar
 
-epsilon = 0.1
-alpha = 0.1
-timesteps = 10_000
+epsilon = 0.1 #Modificar
+alpha = 0.1 #Modificar
+timesteps = 10_000 #Modificar
 
-timestep_of_change = 500
+timestep_of_change = 500 #Modificar
 
 
 run_experiment(alpha,epsilon,timesteps,timestep_of_change,n_actions=K)     
+
+
 
